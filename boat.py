63cfe724c70da5218b722f72532d8888eec2f40f
Администратор <alexushveridze@mail.ru>
2021-12-23 20:40:11 +0300

    improved basic movements (now can't move in 2 directions)

--- boat.py
+++ boat.py
@@ -36,6 +36,9 @@ def boat_run():
     allow_left = False
     allow_up = False
     allow_down = False
+    sliding = False
+    direction = ''
+    sliding_counter = 60
     angle = 0
     speed = 5
     while running:
@@ -44,40 +47,56 @@ def boat_run():
         boat.image = boat_temp_image
         boat.rect = new_rect
         screen.fill('blue')
-        if allow_right:
-            angle -= 2
-        elif allow_left:
+        if allow_left and not allow_right:
             angle += 2
-        if allow_up:
+        if allow_right and not allow_left:
+            angle -= 2
+        if allow_up and not allow_down:
             a = math.radians(-angle)
             b = math.radians(-angle)
             x += math.sin(a) * speed
             y -= math.cos(b) * speed
-        if allow_down:
+        if allow_down and not allow_up:
             a = math.radians(-angle)
             b = math.radians(-angle)
             x -= math.sin(a) * speed
             y += math.cos(b) * speed
+        if sliding and sliding_counter > 0:
+            a = math.radians(-angle)
+            b = math.radians(-angle)
+            if direction == 'up':
+                x += math.sin(a) * sliding_counter * 0.05
+                y -= math.cos(b) * sliding_counter * 0.05
+            elif direction == 'down':
+                x -= math.sin(a) * sliding_counter * 0.05
+                y += math.cos(b) * sliding_counter * 0.05
+            sliding_counter -= 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    allow_right = True
-                elif event.key == pygame.K_LEFT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and not allow_right:
                     allow_left = True
-                elif event.key == pygame.K_UP:
+                if event.key == pygame.K_RIGHT and not allow_left:
+                    allow_right = True
+                if event.key == pygame.K_UP and not allow_down:
                     allow_up = True
-                elif event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and not allow_up:
                     allow_down = True
-            elif event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     allow_right = False
-                elif event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT:
                     allow_left = False
-                elif event.key == pygame.K_UP:
+                if event.key == pygame.K_UP:
+                    direction = 'up'
+                    sliding = True
                     allow_up = False
-                elif event.key == pygame.K_DOWN:
+                    sliding_counter = speed * 10
+                if event.key == pygame.K_DOWN:
+                    direction = 'down'
+                    sliding_counter = speed * 10
+                    sliding = True
                     allow_down = False
         all_sprites.draw(screen)
         pygame.display.flip()
