9a1a02cd5447cdc9694c5fd49c4140fd5134f54a
Администратор <alexushveridze@mail.ru>
2022-01-05 19:41:30 +0300

    added river movement

--- boat.py
+++ boat.py
@@ -30,16 +30,6 @@ class Boat(pygame.sprite.Sprite):
         self.image = self.image.convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
 
-    def move(self, site, multiplier=1):
-        a = math.radians(self.angle)
-        b = math.radians(self.angle)
-        if site == 'up':
-            self.x -= math.sin(a) * speed * multiplier
-            self.y -= math.cos(b) * speed * multiplier
-        if site == 'down':
-            self.x += math.sin(a) * speed * multiplier
-            self.y += math.cos(b) * speed * multiplier
-
     def rotate(self):
         boat_temp_image = pygame.transform.rotate(boat_image, self.angle)
         new_rect = boat_temp_image.get_rect(center=(self.x, self.y))
@@ -55,11 +45,21 @@ class Boat(pygame.sprite.Sprite):
 class River(pygame.sprite.Sprite):
     def __init__(self, polygon):
         super(River, self).__init__(river_sprites)
-        self.image = pygame.Surface([width,height], pygame.SRCALPHA, 32)
+        self.image = pygame.Surface([5000,5000], pygame.SRCALPHA, 32)
         pygame.draw.polygon(self.image, 'blue', (polygon))
         self.rect = self.image.get_rect()
         self.image = self.image.convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self, site, angle, multiplier=1):
+        a = math.radians(angle)
+        b = math.radians(angle)
+        if site == 'down':
+            self.rect.x -= math.sin(a) * speed * multiplier
+            self.rect.y -= math.cos(b) * speed * multiplier
+        if site == 'up':
+            self.rect.x += math.sin(a) * speed * multiplier
+            self.rect.y += math.cos(b) * speed * multiplier
 
 
 boat_image = load_image('boat.png')
@@ -73,7 +73,7 @@ def boat_run():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("boat")
     screen.fill('orange')
-    river = River([(300, 300), (750, 400), (750, 650), (300, 750)])  # example
+    river = River([(465, 188), (864, 224), (837, 527), (974, 718), (1267, 688), (1807, 1372), (1988, 1391), (2056, 1461), (2072, 1488), (2147, 1578), (2153, 2504), (2202, 2586), (2159, 2891), (2136, 2984), (2432, 3246), (2456, 3367), (2449, 3402), (2567, 3612), (2869, 3852), (2878, 4077), (3404, 4104), (3598, 4605), (3531, 5016), (3136, 4952), (3185, 4648), (3125, 4490), (2493, 4458), (2477, 4051), (2257, 3877), (2029, 3470), (2037, 3431), (1690, 3123), (1765, 2816), (1786, 2668), (1724, 2563), (1754, 2507), (1749, 1768), (1597, 1752), (1089, 1108), (784, 1139), (425, 638), (465, 188)])  # example
     boat = Boat(boat_image)
     all_sprites.add(boat)
     clock = pygame.time.Clock()
@@ -82,8 +82,6 @@ def boat_run():
     allow_left = False
     allow_up = False
     allow_down = False
-    sliding = False
-    direction = ''
     while running:
         boat.rotate()
         screen.fill('orange')
@@ -92,12 +90,12 @@ def boat_run():
         if allow_right:
             boat.angle -= 2
         if allow_up:
-            boat.move('up')
+            river.move('up', boat.angle)
         if allow_down:
-            boat.move('down')
-        if sliding and multiplier > 0:
-            boat.move(direction, multiplier)
-            multiplier -= 0.01
+            river.move('down', boat.angle)
+        #if sliding and multiplier > 0:
+        #    river.move(direction, boat.angle, multiplier)
+        #    multiplier -= 0.01
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -116,14 +114,8 @@ def boat_run():
                 if event.key == pygame.K_LEFT:
                     allow_left = False
                 if event.key == pygame.K_UP:
-                    direction = 'up'
-                    sliding = True
                     allow_up = False
-                    multiplier = 0.5
                 if event.key == pygame.K_DOWN:
-                    direction = 'down'
-                    multiplier = 0.5
-                    sliding = True
                     allow_down = False
         boat.update(river)
         river_sprites.draw(screen)
