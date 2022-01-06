08ec93258a8aa92a3d85b95e0196f8bdf074250f
Администратор <alexushveridze@mail.ru>
2022-01-06 20:22:44 +0300

    improved code

--- boat.py
+++ boat.py
@@ -7,6 +7,8 @@ import os
 size = width, height = 1000, 800
 all_sprites = pygame.sprite.Group()
 river_sprites = pygame.sprite.Group()
+beach_size = 1000
+river_size = 1000
 
 def load_image(path):
     full_path = os.path.join('data', path)
@@ -37,8 +39,8 @@ class Boat(pygame.sprite.Sprite):
         self.rect = new_rect
 
 
-    def update(self, river, beach1, beach2):
-        if not pygame.sprite.collide_mask(self, river):
+    def update(self, beach1, beach2, river):
+        if not pygame.sprite.collide_mask(self, beach1):
             exit()
         if pygame.sprite.collide_mask(self, beach1) or pygame.sprite.collide_mask(self, beach2):
             exit()
@@ -64,9 +66,16 @@ class River(pygame.sprite.Sprite):
             self.rect.y += math.cos(b) * speed * multiplier
 
 class Beach(River):
-    def __init__(self, polygon):
+    def __init__(self, polygon, site):
         super(River, self).__init__(river_sprites)
         self.image = pygame.Surface([5000, 5000], pygame.SRCALPHA, 32)
+        if site == 'right':
+            polygon = [(beach_size, beach_size)] + polygon
+            polygon = polygon + [(beach_size, 0)]
+        if site == 'left':
+            polygon = [(0, beach_size)] + polygon
+            polygon = polygon + [(0, 0)]
+        print(polygon)
         pygame.draw.polygon(self.image, 'orange', (polygon))
         self.rect = self.image.get_rect()
         self.image = self.image.convert_alpha()
@@ -83,10 +92,10 @@ def boat_run():
     multiplier = 1
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("boat")
-    screen.fill('orange')
-    river = River([(465, 188), (864, 224), (837, 527), (974, 718), (1267, 688), (1807, 1372), (1988, 1391), (2056, 1461), (2072, 1488), (2147, 1578), (2153, 2504), (2202, 2586), (2159, 2891), (2136, 2984), (2432, 3246), (2456, 3367), (2449, 3402), (2567, 3612), (2869, 3852), (2878, 4077), (3404, 4104), (3598, 4605), (3531, 5016), (3136, 4952), (3185, 4648), (3125, 4490), (2493, 4458), (2477, 4051), (2257, 3877), (2029, 3470), (2037, 3431), (1690, 3123), (1765, 2816), (1786, 2668), (1724, 2563), (1754, 2507), (1749, 1768), (1597, 1752), (1089, 1108), (784, 1139), (425, 638), (465, 188)])  # example
-    beach1 = Beach()
-    beach2 = Beach()
+    screen.fill('blue')
+    river = River()  # сюда полигон реки
+    beach1 = Beach() # сюда правого берега
+    beach2 = Beach() # сюда левого береша
     boat = Boat(boat_image)
     all_sprites.add(boat)
     clock = pygame.time.Clock()
@@ -95,16 +104,21 @@ def boat_run():
     allow_left = False
     allow_up = False
     allow_down = False
+    cnt = 200
     while running:
         boat.rotate()
-        screen.fill('orange')
+        screen.fill('blue')
         if allow_left:
             boat.angle += 2
         if allow_right:
             boat.angle -= 2
         if allow_up:
+            beach1.move('up', boat.angle)
+            beach2.move('up', boat.angle)
             river.move('up', boat.angle)
         if allow_down:
+            beach1.move('down', boat.angle)
+            beach2.move('down', boat.angle)
             river.move('down', boat.angle)
         #if sliding and multiplier > 0:
         #    river.move(direction, boat.angle, multiplier)
@@ -130,7 +144,10 @@ def boat_run():
                     allow_up = False
                 if event.key == pygame.K_DOWN:
                     allow_down = False
-        boat.update(river)
+        if cnt > 0:
+            cnt -= 1
+        else:
+            boat.update(beach1, beach2, river)
         river_sprites.draw(screen)
         all_sprites.draw(screen)
         pygame.display.flip()
